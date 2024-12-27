2de748aa647f60a6e445631968935ca2f085d0a4
Mattias Holm <mattias.holm@live.com>
2024-12-27 09:38:31 +0100

    Added Docusaurus project

---
+++ docusaurus/docusaurus.config.ts
@@ -0,0 +1,70 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Drinks',
+  tagline: 'A collection of cocktails by a hobby mixologist',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://mattiasholm.github.io',
+  baseUrl: '/',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          path: '../drinks',
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+    },
+    navbar: {
+      logo: {
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          label: 'Drinks',
+          type: 'docSidebar',
+          sidebarId: 'defaultSidebar',
+        },
+        {
+          label: 'GitHub',
+          href: 'https://github.com/mattiasholm/drinks',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Mattias Holm`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ docusaurus/src/pages/index.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className='container'>
+        <Heading as='h1' className='hero__title'>
+          {siteConfig.title} 🍸
+        </Heading>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className='button button--secondary button--lg'
+            to='/Airmail'> {/* Temporarily hardcoded, grab first or RANDOM filename dynamically instead VS built in API to link to docSidebar! */}
+            Discover
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
